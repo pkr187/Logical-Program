c33b37640c8e61d2143c93beb5ed4f239efc31c5
pkr187 <pankajroman187@gmail.com>
2022-09-09 22:45:21 +0530

    [pankaj] Add:TemperatureConversion

---
+++ TemperatureConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TemperatureConversion
+{
+    internal class TemperatureConversion
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConversion  temp = new TemperatureConversion();
+            Console.Write("please provide celsius Temp");
+            string val1 = Console.ReadLine();
+            double Fehrenheit = temp.CelsiusToFehrenheit(val1);
+            Console.WriteLine($"the value to {val1} celsius is {Fehrenheit}Fehrenheit");
+
+            Console.Write("please provide celsius Temp:");
+            string val2 = Console.ReadLine();
+            double celsius = temp.CelsiusToFehrenheit(val2);
+            Console.WriteLine($"the value to {val2} celsius is {celsius}Fehrenheit");
+        }
+
+        private double CelsiusToFehrenheit(string? val2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+        
+    
---
+++ TemperatureConversion/TemperatureConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConversion
+{
+    internal class program
+    {
+        public double CelsiusToFehrenheit(String celsiusTemp)
+        {
+            double celsious = double.Parse(celsiusTemp);
+            double Fehrenheit = (celsious * 9 / 5) + 32;
+            return Fehrenheit;
+        }
+        public double FehrenheitToFehrenheit(String FehrenheitTemp)
+        {
+            double Fehrenheit = double.Parse(FehrenheitTemp);
+            double celsius = (Fehrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+
+
+    }
+}
+
