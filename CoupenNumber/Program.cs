7c953fa1253a3a52dcb59397b0ab1d2f1e332277
pkr187 <pankajroman187@gmail.com>
2022-09-09 23:10:07 +0530

    [pankaj] Add:CoupenNumber

---
+++ CoupenNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace CoupenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ALPHABET = "AG8FOLE2WVTCPY5ZH3NIUDBXSMQK7946";
+            static string couponCode(uint number)
+            {
+                StringBuilder b = new StringBuilder();
+                for (int i = 0; i < 6; ++i)
+                {
+                    b.Append(ALPHABET[(int)number & ((1 << 5) - 1)]);
+                    number = number >> 5;
+                }
+                return b.ToString();
+            }
+            static uint codeFromCoupon(string coupon)
+            {
+                uint n = 0;
+                for (int i = 0; i < 6; ++i)
+                    n = n | (((uint)ALPHABET.IndexOf(coupon[i])) << (5 * i));
+                return n;
+            }
+        }
+    }
+}
