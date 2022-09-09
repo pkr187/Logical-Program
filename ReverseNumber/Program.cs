5c08802a5d35392bb2ac3c2006b346376706808e
pkr187 <pankajroman187@gmail.com>
2022-09-09 22:23:42 +0530

    [pankaj] Add: ReverseNumber

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123456789;
+            int result = 0;
+            int rem;
+            while (num != 0)
+            {
+                rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
+       
+    
