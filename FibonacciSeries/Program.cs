c7d0962661aa78e20c379e094a961f7169d1e0ca
pkr187 <pankajroman187@gmail.com>
2022-09-09 22:06:03 +0530

    [pankaj] Add:Fibonacci-Series

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1, num3, range;
+            Console.WriteLine("Enter tne range : ");
+            range = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + " ");
+            Console.WriteLine(num2 + " ");
+            while (range > 1)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+                range--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
